c63ae68fdc9c75de5d9208f1ef7b91bb5e0e8844
Frits Hoogland <frits.hoogland@gmail.com>
2018-02-04 19:45:48 +0000

    kcrf functions mainly

--- sf.sh
+++ sf.sh
@@ -35,34 +35,34 @@ else
 	WILDCARD=no
 fi
 FUNCTION=$1
-sqlite3 memory_ranges.db "drop table if exists functions;"
-sqlite3 memory_ranges.db "create table functions (function_name varchar(30), function_sum varchar(60));"
-sqlite3 memory_ranges.db ".import functions.csv functions"
-MIN_LENGTH=$(sqlite3 memory_ranges.db "select min(length(function_name)) from functions;")
-MAX_LENGTH=$(sqlite3 memory_ranges.db "select max(length(function_name)) from functions;")
+sqlite3 functions.db "drop table if exists functions;"
+sqlite3 functions.db "create table functions (function_name varchar(30), function_sum varchar(60));"
+sqlite3 functions.db ".import functions.csv functions"
+MIN_LENGTH=$(sqlite3 functions.db "select min(length(function_name)) from functions;")
+MAX_LENGTH=$(sqlite3 functions.db "select max(length(function_name)) from functions;")
 function_description() {
         LOCAL_FUNCTION_NAME=$(echo $1 | sed 's/_/\\_/g')
         USE=$2
         [ ${#LOCAL_FUNCTION_NAME} -lt $MAX_LENGTH ] && LOCAL_LENGTH=${#LOCAL_FUNCTION_NAME} || LOCAL_LENGTH=$MAX_LENGTH
         if [ $USE = "n" ]; then
-        	RETURN=$(for LEN in $(seq $MIN_LENGTH $LOCAL_LENGTH); do sqlite3 memory_ranges.db "select function_sum from functions where function_name like substr('$LOCAL_FUNCTION_NAME',1,$LEN) escape '\';"; done)
+        	RETURN=$(for LEN in $(seq $MIN_LENGTH $LOCAL_LENGTH); do sqlite3 functions.db "select function_sum from functions where function_name like substr('$LOCAL_FUNCTION_NAME',1,$LEN) escape '\';"; done)
         elif [ $USE = "r" ]; then
                 RETURN=0
 		PREV_RESULT=""
         	for LEN in $(seq $MIN_LENGTH $LOCAL_LENGTH); do
-			RESULT="$RESULT$(sqlite3 memory_ranges.db "select function_sum from functions where function_name like substr('$LOCAL_FUNCTION_NAME',1,$LEN) escape '\';")"
+			RESULT="$RESULT$(sqlite3 functions.db "select function_sum from functions where function_name like substr('$LOCAL_FUNCTION_NAME',1,$LEN) escape '\';")"
 			if [ "${#RESULT}" -gt "${#PREV_RESULT}" ]; then 
 				RETURN=$LEN
 				PREV_RESULT=$RESULT
 			fi
 		done
 	elif [ $USE = "l" ]; then
-		printf "$(sqlite3 memory_ranges.db "select function_name||'\t'||function_sum||'\n' from functions where function_name like '${LOCAL_FUNCTION_NAME}%' and length(function_name) <= $LEVEL;")" | while read OUT; do
+		printf "$(sqlite3 functions.db "select function_name||'\t'||function_sum||'\n' from functions where function_name like '${LOCAL_FUNCTION_NAME}%' and length(function_name) <= $LEVEL;")" | while read OUT; do
 			echo "$OUT"
 		done
 		RETURN=0
 	elif [ $USE = "w" ]; then
-		printf "$(sqlite3 memory_ranges.db "select function_name||'\t'||function_sum||'\n' from functions where function_name like '${LOCAL_FUNCTION_NAME}%';")" | while read OUT; do
+		printf "$(sqlite3 functions.db "select function_name||'\t'||function_sum||'\n' from functions where function_name like '${LOCAL_FUNCTION_NAME}%';")" | while read OUT; do
 			echo "$OUT"
 		done
 		RETURN=0
